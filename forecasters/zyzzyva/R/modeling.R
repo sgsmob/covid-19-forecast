1d758e4dd42499775017b55363ca658fc216da73
Mike O'Brien <mikeob@google.com>
2021-02-22 20:57:12 -0500

    allow calibration of results

--- forecasters/zyzzyva/R/modeling.R
+++ forecasters/zyzzyva/R/modeling.R
@@ -15,31 +15,33 @@ ml.fit_model <- function(train_test,
   }
 }
 
-ml.extract_slope_and_lag_columns <- function(X, base_covariates, location_covariates) {
+ml.extract_slope_and_lag_columns_fn <- function(base_covariates, location_covariates) {
   # create new_train_X, new_test_X with slope variables
   slope_base_vars <- paste(sapply(base_covariates, function(x) x$name),
                            "lag", sep = "_")
-  new_X <- matrix(NA, nrow=nrow(X), ncol=length(slope_base_vars))
-  for (i in 1:length(slope_base_vars)) {
-    var_cols <- which(startsWith(colnames(X), slope_base_vars[i]))
-    n_var_cols <- length(var_cols)
-    if (n_var_cols <= 1) {
-      stop("Could not create slope vars in training X, not enough columns for ", var)
-    }
-    new_X[,i] <- stats::lsfit(-(1:n_var_cols), t(X[, var_cols]))$coef[2,]
-  }
-  # add first lags of handpicked variables
   location_covariate_names <- sapply(location_covariates, function(x) x$name)
-  first_cases <- sapply(c(slope_base_vars, location_covariate_names),
-                        function(var) which(startsWith(colnames(X), var))[1],
-                        USE.NAMES = F)
-  new_X <- cbind(new_X, X[, first_cases])
-  
-  covariate_names <- c(paste(slope_base_vars, "slope", sep="_"),
-                       colnames(X)[first_cases])
-  
-  colnames(new_X) <- covariate_names
-  return(new_X)
+  function(X) {
+    new_X <- matrix(NA, nrow=nrow(X), ncol=length(slope_base_vars))
+    for (i in 1:length(slope_base_vars)) {
+      var_cols <- which(startsWith(colnames(X), slope_base_vars[i]))
+      n_var_cols <- length(var_cols)
+      if (n_var_cols <= 1) {
+        stop("Could not create slope vars in training X, not enough columns for ", var)
+      }
+      new_X[,i] <- stats::lsfit(-(1:n_var_cols), t(X[, var_cols]))$coef[2,]
+    }
+    # add first lags of handpicked variables
+    first_cases <- sapply(c(slope_base_vars, location_covariate_names),
+                          function(var) which(startsWith(colnames(X), var))[1],
+                          USE.NAMES = F)
+    new_X <- cbind(new_X, X[, first_cases])
+    
+    covariate_names <- c(paste(slope_base_vars, "slope", sep="_"),
+                        colnames(X)[first_cases])
+    
+    colnames(new_X) <- covariate_names
+    return(new_X)
+  }
 }
 
 
@@ -69,43 +71,52 @@ ml.linear <- function(train_test,
   test_row_locations <- train_test$test_row_locations[[1]]
 
   # create new_train_X, new_test_X with slope variables
-  new_train_X <- ml.extract_slope_and_lag_columns(train_X,
-                                                  modeling_options$base_covariates,
-                                                  modeling_options$location_covariates)
-  new_test_X <- ml.extract_slope_and_lag_columns(test_X,
-                                                 modeling_options$base_covariates,
-                                                 modeling_options$location_covariates)
+  extract_slope_and_lag_columns <-
+    ml.extract_slope_and_lag_columns_fn(modeling_options$base_covariates,
+                                        modeling_options$location_covariates)
+  new_train_X <- extract_slope_and_lag_columns(train_X)
+  new_test_X <- extract_slope_and_lag_columns(test_X)
   logger::log_debug("Input colnames:\n",
                     paste(colnames(new_train_X), collapse='\n'))
   
   # finally, get predictions
-  out <- matrix(NA, nrow=nrow(test_X), ncol=length(modeling_options$quantiles))
-  # get quantiles
-  fit_quantiles <- quantgen::quantile_lasso(new_train_X,
-                                            train_y,
-                                            tau = modeling_options$quantiles,
-                                            standardize = FALSE,
-                                            lambda = 0)
-  preds <-  predict(fit_quantiles, new_test_X, sort = TRUE)
+  tau <- modeling_options$quantiles
 
-  if (FALSE) {
-  # if (modeling_options$recalibrate) {
+  if (modeling_options$recalibrate) {
+    # separate out a validation set
+    n <- nrow(train_X)
+    fit_inds <- sample(1:n, floor(0.75 * n), replace = FALSE)
+    fit_train_y <- train_y[fit_inds,]
+    fit_train_X <- extract_slope_and_lag_columns(train_X[fit_inds,])
+    validate_train_y <- train_y[-fit_inds,]
+    validate_train_X <- extract_slope_and_lag_columns(train_X[-fit_inds,])
+
+    fit_quantiles <- quantgen::quantile_lasso(fit_train_X,
+                                              fit_train_y,
+                                              tau = tau,
+                                              standardize = FALSE,
+                                              lambda = 0)
+    preds <- predict(fit_quantiles, validate_train_X, sort = TRUE)
     # isotonic regression to ensure quantiles ordering
     for (i in 1:nrow(preds)) {
       preds[i, ] <- Iso::pava(preds[i, ])
     }
 
-    tau_calibrated <- ca.get_empirical_quantiles(modeling_options$quantiles,
-                                                 preds,
-                                                 train_y)
-    fit_quantiles <- quantgen::quantile_lasso(new_train_X,
-                                          train_y,
-                                          tau = tau_calibrated,
-                                          standardize = FALSE,
-                                          lambda = 0)
-    preds <- predict(fit_quantiles, new_test_X, sort = TRUE)
+    tau <- ca.get_empirical_quantiles(tau,
+                                      preds,
+                                      validate_train_y)
+    print(tau)
   }
 
+  out <- matrix(NA, nrow=nrow(new_test_X), ncol=length(tau))
+
+  # get quantiles
+  fit_quantiles <- quantgen::quantile_lasso(new_train_X,
+                                            train_y,
+                                            tau = tau,
+                                            standardize = FALSE,
+                                            lambda = 0)
+  preds <- predict(fit_quantiles, new_test_X, sort = TRUE)
   colnames(preds) <- modeling_options$quantiles
   out <- preds
   
